cd0b6d610c932da795699949119fbfaae01f762a
Richard Turner <rwturner @ stanford.edu>
2013-05-19 15:14:28 -0400

    Started Slower Class

---
+++ Slower.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 19 15:13:50 2013
+
+@author: Will
+"""
+
+class Slower():
+    pass
+
+class ZeemanSlower(Slower):
+    pass
+
+class ChirpedSlower(Slower):
+    pass
